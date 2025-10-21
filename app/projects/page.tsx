59aa2d62087f02b7e268c9df519f497c28a4fc16
Murunwa <134610001+MurunwaMaphiri1@users.noreply.github.com>
2025-10-22 00:23:46 +0200

    Added new project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,8 +17,22 @@ type ProjectItem = {
 
 
 const projectItems: ProjectItem[] = [
-  {
+    {
     id: 1,
+    name: "GraceNotes (Bible Study Application)",
+    timeline: "Oct 2025",
+    description: "A personal blog for deep Bible studies. The content is managed via MDX files and dynamically rendered by a fast, server-side-rendered frontend built with NextJS and TypeScript.",
+    githublink: "https://github.com/MurunwaMaphiri1/bible-study-project-v2",
+    livedemo: "https://bible-study-project-v2.vercel.app/",
+    techstack: [
+      "NextJS",
+      "TypeScript",
+      "MDX",
+    ],
+    image: "/images/project-videos/GraceNotes.png",
+  },
+  {
+    id: 2,
     name: "MagicBox Theatres",
     timeline: "Feb 2025 - Mar 2025",
     description: "The Movie Reservation System is a .NET Core Web API that allows users to browse movies, select showtimes and book reservations securely. The API is designed with JWT authentication, Entity Framework Core and PostgreSQL for data storage. Also includes email functionality to confirm reservation.",
@@ -33,7 +47,7 @@ const projectItems: ProjectItem[] = [
     image: "/images/project-videos/MagicBox Theatres.png",
   },
   {
-    id: 2,
+    id: 3,
     name: "E-commerce app",
     timeline: "Jan 2025 - Feb 2025",
     description: "This project is an API for an e-commerce platform that allows users to sign up, log in, add products to a shopping cart, remove products, view and search for products, and checkout with a payment gateway. The backend includes JWT authentication for secure user interaction and integrates with Stripe for handling payments.",
@@ -48,7 +62,7 @@ const projectItems: ProjectItem[] = [
     image: "/images/project-videos/e-commerce app.png",
   },
   {
-    id: 3,
+    id: 4,
     name: "YumeAnime(夢アニメ)",
     timeline: "Dec 2024 - Jan 2025",
     description: "Integrated Jikan API to display seasonal, upcoming and top anime.",
