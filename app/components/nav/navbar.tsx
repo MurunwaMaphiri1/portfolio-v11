8bdae210543b4413366b061021e4c859a9ba586a
Murunwa <134610001+MurunwaMaphiri1@users.noreply.github.com>
2025-11-20 22:46:21 +0200

    Changed CV

--- app/components/nav/navbar.tsx
+++ app/components/nav/navbar.tsx
@@ -20,7 +20,7 @@ const NavBar = () => {
         <a href="/projects">
           <FolderGit height={20} width={20}/>
         </a>
-        <a href="https://drive.google.com/file/d/1Fvg2O0fam646EMnBgV68GTOm51GdHaSr/view?usp=sharing" download>
+        <a href="https://drive.google.com/file/d/1V6V-yJec4Q6S2Q5J8px2D3Z1RFfmjS2y/view?usp=sharing" download>
           <FileUser height={20} width={20}/>
         </a>
       </div>
